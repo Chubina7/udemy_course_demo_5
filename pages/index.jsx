14db72f566ec1dff9b50e3dccb82b71538814c50
Luka Chubinidze <chubinidzel08@gmail.com>
2024-01-26 16:50:28 +0400

    add featured posts grid and its items

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import Hero from "@/components/homePage/Hero";
+import FeaturedPosts from "@/components/homePage/FeaturedPosts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-next-js",
+    title: "Getting started with NEXT.JS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NEXT.JS is REACT framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2024-01-26",
+  },
+  {
+    slug: "getting-started-with-next-js2",
+    title: "Getting started with NEXT.JS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NEXT.JS is REACT framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2024-01-26",
+  },
+  {
+    slug: "getting-started-with-next-js3",
+    title: "Getting started with NEXT.JS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NEXT.JS is REACT framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2024-01-26",
+  },
+];
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      {/* <FeaturedPosts /> */}
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
   );
 };
