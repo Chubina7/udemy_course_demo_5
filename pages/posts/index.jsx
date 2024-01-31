803d478b15bbd33eefacf5ddc025001f329b3702
Luka Chubinidze <chubinidzel08@gmail.com>
2024-01-31 15:33:00 +0400

    add all posts page

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -1,7 +1,35 @@
+import AllPosts from "@/components/posts/AllPosts";
 import React from "react";
 
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-next-js",
+    title: "Getting started with NEXT.JS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NEXT.JS is REACT framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2024-01-26",
+  },
+  {
+    slug: "getting-started-with-next-js2",
+    title: "Getting started with NEXT.JS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NEXT.JS is REACT framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2024-01-26",
+  },
+  {
+    slug: "getting-started-with-next-js3",
+    title: "Getting started with NEXT.JS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NEXT.JS is REACT framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.",
+    date: "2024-01-26",
+  },
+];
+
 const AllPostsPage = () => {
-  return <div>All posts page</div>;
+  return <AllPosts posts={DUMMY_POSTS} />;
 };
 
 export default AllPostsPage;
