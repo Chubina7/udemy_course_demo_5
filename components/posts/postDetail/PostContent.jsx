87931bc697ed13cbd290576adab07a41614d9ded
Luka Chubinidze <chubinidzel08@gmail.com>
2024-02-01 12:34:05 +0400

    handled image render from markdown file

--- components/posts/postDetail/PostContent.jsx
+++ components/posts/postDetail/PostContent.jsx
@@ -2,13 +2,39 @@ import ReactMarkdown from "react-markdown";
 import React from "react";
 import PostHeader from "./PostHeader";
 import styles from "./PostContent.module.css";
+import Image from "next/image";
 
 function PostContent({ post }) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+              layout="responsive"
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={styles.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
